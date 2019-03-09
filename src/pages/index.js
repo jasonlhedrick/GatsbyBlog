4c0e7aa70c5a5e0fcc2b69221002cfda284119d8
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-09 11:26:12 -0800

    Updated index.js to make sure changes took.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/labs-1/">Labs 1 - Start Up</Link>
   </Layout>
 )
 
